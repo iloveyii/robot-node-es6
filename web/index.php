d9b1911b7677a01b8f0d5f482a93dff326f7f918
Hazrat Ali <iloveyii@yahoo.com>
2019-01-08 01:29:14 +0100

    Added shell script - expect send

--- web/index.php
+++ web/index.php
@@ -6,17 +6,14 @@ header("Content-Type: application/json");
 $result['status'] = true;
 $result['output'] = 'cmd result';
 
-$command = "2
-1 1
-N 3
-W 4";
+$command = "2 \n1 1\nN 3\nW 4";
 
 $path = realpath( dirname(__FILE__) . '/../public/bundle.js') ;
 
-$runit = " echo '$command' ";
+echo $runit = "node $path < $command";
 
 exec($runit, $output);
 
-print_r($output);
+// print_r($output);
 // echo json_encode($result);
 
